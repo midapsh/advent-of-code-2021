fc2c0d10f345935febc3dd7d33a24ae89f4b96e9
hspadim <henrique@spadim.com.br>
2022-02-23 23:17:20 -0300

    Add first part of day 2

---
+++ src/day2.rs
@@ -0,0 +1,44 @@
+pub const DUMMY_INPUT: &str = include_str!("data/day2-dummy.txt");
+pub const REAL_INPUT: &str = include_str!("data/day2-real.txt");
+
+pub fn solve_part_1(values: &str) -> String {
+    let mut x = 0;
+    let mut y = 0;
+
+    values.lines().for_each(|line| {
+        let mut iter = line.trim().split_ascii_whitespace();
+        let command = iter.next();
+        let gain = iter.next().unwrap().parse::<i32>().unwrap();
+
+        match command {
+            Some("up") => y -= gain,
+            Some("down") => y += gain,
+            Some("forward") => x += gain,
+            _ => panic!("Not a valid command"),
+        }
+    });
+    (x * y).to_string()
+}
+
+pub fn solve_part_2(values: &str) -> String {
+    unimplemented!()
+}
+
+fn main() {
+    println!("{}", solve_part_1(REAL_INPUT));
+    // println!("{}", solve_part_2(REAL_INPUT));
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{solve_part_1, solve_part_2, DUMMY_INPUT};
+
+    #[test]
+    fn test_part_1() {
+        assert_eq!("150", solve_part_1(DUMMY_INPUT));
+    }
+    // #[test]
+    // fn test_part_2() {
+    //     assert_eq!("5", solve_part_2(DUMMY_INPUT));
+    // }
+}
