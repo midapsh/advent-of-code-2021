b711d0d84b6a35a850e83b6b7896cb1acff39106
hspadim <henrique@spadim.com.br>
2022-02-23 23:27:23 -0300

    Add part 2 of day 2

--- src/day2.rs
+++ src/day2.rs
@@ -21,12 +21,31 @@ pub fn solve_part_1(values: &str) -> String {
 }
 
 pub fn solve_part_2(values: &str) -> String {
-    unimplemented!()
+    let mut x = 0;
+    let mut y = 0;
+    let mut aim = 0;
+
+    values.lines().for_each(|line| {
+        let mut iter = line.trim().split_ascii_whitespace();
+        let command = iter.next();
+        let gain = iter.next().unwrap().parse::<i32>().unwrap();
+
+        match command {
+            Some("up") => aim -= gain,
+            Some("down") => aim += gain,
+            Some("forward") => {
+                x += gain;
+                y += aim * gain;
+            }
+            _ => panic!("Not a valid command"),
+        }
+    });
+    (x * y).to_string()
 }
 
 fn main() {
     println!("{}", solve_part_1(REAL_INPUT));
-    // println!("{}", solve_part_2(REAL_INPUT));
+    println!("{}", solve_part_2(REAL_INPUT));
 }
 
 #[cfg(test)]
@@ -37,8 +56,8 @@ mod tests {
     fn test_part_1() {
         assert_eq!("150", solve_part_1(DUMMY_INPUT));
     }
-    // #[test]
-    // fn test_part_2() {
-    //     assert_eq!("5", solve_part_2(DUMMY_INPUT));
-    // }
+    #[test]
+    fn test_part_2() {
+        assert_eq!("900", solve_part_2(DUMMY_INPUT));
+    }
 }
