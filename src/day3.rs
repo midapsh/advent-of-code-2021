cda4fcd6b0b246ff074cf6ffec8c043f82aecf54
hspadim <henrique@spadim.com.br>
2022-02-25 23:05:46 -0300

    Stop day3 part 2 for a while

--- src/day3.rs
+++ src/day3.rs
@@ -1,7 +1,7 @@
 const _DUMMY_INPUT: &str = include_str!("data/day3-dummy.txt");
 const REAL_INPUT: &str = include_str!("data/day3-real.txt");
 
-pub fn private_solve_part_1(values: &str) -> String {
+fn private_solve_part_1(values: &str) -> String {
     let mut count_lines = 0;
     let mut counter: Vec<u32> = Vec::new();
     const RADIX: u32 = 10;
@@ -34,8 +34,45 @@ pub fn private_solve_part_1(values: &str) -> String {
     (gamma_rate * epsilon_rate).to_string()
 }
 
-pub fn private_solve_part_2(values: &str) -> String {
-    String::new()
+fn private_solve_part_2(values: &str) -> String {
+    let numbers = values
+        .lines()
+        .map(|line| {
+            line.trim()
+                .chars()
+                .map(|char| char != '0')
+                .collect::<Vec<_>>()
+        })
+        .collect::<Vec<_>>();
+
+    let o2 = extract_number(numbers.clone(), |one_bits, total_bits| {
+        one_bits * 2 >= total_bits
+    });
+    let co2 = extract_number(numbers.clone(), |one_bits, total_bits| {
+        one_bits * 2 < total_bits
+    });
+    (o2 * co2).to_string()
+}
+
+fn extract_number(mut numbers: Vec<Vec<bool>>, criteria: fn(usize, usize) -> bool) -> i32 {
+    for pos in 0..numbers[0].len() {
+        if numbers.len() == 1 {
+            break;
+        }
+        number_filter(&mut numbers, pos, criteria);
+    }
+    numbers[0]
+        .iter()
+        .fold(0, |value, &bit| (value << 1) + if bit { 1 } else { 0 })
+}
+
+fn number_filter(numbers: &mut Vec<Vec<bool>>, pos: usize, criteria: fn(usize, usize) -> bool) {
+    let num_ones = numbers
+        .iter()
+        .fold(0, |n, number| if number[pos] { n + 1 } else { n });
+    let bit = criteria(num_ones, numbers.len());
+    // numbers.retain()
+    todo!()
 }
 
 fn _solve_part_1_dummy() -> String {
