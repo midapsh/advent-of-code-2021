81d6956a9c5ca0cfd774f06d4da26fe1aad85ce1
hspadim <henrique@spadim.com.br>
2022-03-03 00:55:05 -0300

    Remove unused code

--- src/day6.rs
+++ src/day6.rs
@@ -3,95 +3,6 @@ const REAL_INPUT: &str = include_str!("data/day6-real.txt");
 const PART_1_NUMBER_OF_DAYS: usize = 80;
 const PART_2_NUMBER_OF_DAYS: usize = 256;
 
-struct FishStates(u64, u64, u64, u64, u64, u64, u64, u64, u64);
-
-impl FishStates {
-    fn new() -> Self {
-        Self(0, 0, 0, 0, 0, 0, 0, 0, 0)
-    }
-    fn get(&self, key: usize) -> u64 {
-        match key {
-            0 => self.0,
-            1 => self.1,
-            2 => self.2,
-            3 => self.3,
-            4 => self.4,
-            5 => self.5,
-            6 => self.6,
-            7 => self.7,
-            8 => self.8,
-            _ => panic!("Not a valid value"),
-        }
-    }
-    fn insert(&mut self, key: usize, value: u64) {
-        match key {
-            0 => {
-                self.0 = value;
-            }
-            1 => {
-                self.1 = value;
-            }
-            2 => {
-                self.2 = value;
-            }
-            3 => {
-                self.3 = value;
-            }
-            4 => {
-                self.4 = value;
-            }
-            5 => {
-                self.5 = value;
-            }
-            6 => {
-                self.6 = value;
-            }
-            7 => {
-                self.7 = value;
-            }
-            8 => {
-                self.8 = value;
-            }
-            _ => panic!("Not a valid value"),
-        }
-    }
-    fn add(&mut self, key: usize, value: u64) {
-        match key {
-            0 => {
-                self.0 += value;
-            }
-            1 => {
-                self.1 += value;
-            }
-            2 => {
-                self.2 += value;
-            }
-            3 => {
-                self.3 += value;
-            }
-            4 => {
-                self.4 += value;
-            }
-            5 => {
-                self.5 += value;
-            }
-            6 => {
-                self.6 += value;
-            }
-            7 => {
-                self.7 += value;
-            }
-            8 => {
-                self.8 += value;
-            }
-            _ => panic!("Not a valid value"),
-        }
-    }
-    fn get_total_values(self) -> u64 {
-        self.0 + self.1 + self.2 + self.3 + self.4 + self.5 + self.6 + self.7 + self.8
-    }
-}
-
 fn private_solve_part_1(values: &str) -> u64 {
     solution_via_array(values, PART_1_NUMBER_OF_DAYS)
 }
@@ -128,31 +39,6 @@ fn get_counters(values: &str) -> [u64; 9] {
 
 fn private_solve_part_2(values: &str) -> u64 {
     solution_via_array(values, PART_2_NUMBER_OF_DAYS)
-    // solution_via_struct(values, PART_2_NUMBER_OF_DAYS)
-}
-
-fn solution_via_struct(values: &str, days: usize) -> u64 {
-    let mut fish_states = FishStates::new();
-    values
-        .lines()
-        .next()
-        .unwrap()
-        .trim()
-        .split(',')
-        .map(|number| number.parse::<usize>().unwrap())
-        .for_each(|x| {
-            fish_states.add(x, 1);
-        });
-    for _ in 0..days {
-        let count = fish_states.0;
-        fish_states.insert(0, fish_states.1);
-        for fish_state in 0..8 {
-            fish_states.insert(fish_state, fish_states.get(fish_state + 1));
-        }
-        fish_states.add(6, count);
-        fish_states.insert(8, count);
-    }
-    fish_states.get_total_values()
 }
 
 fn _solve_part_1_dummy() -> u64 {
